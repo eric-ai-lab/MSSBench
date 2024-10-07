a875bd97588c1995ee79f14e64ce542b50e17612
Lczzzzzzzz <115693550+Chenngzz@users.noreply.github.com>
2024-10-08 03:36:45 +0800

    Update load_mPLUG-Owl2.py

--- models/load_mPLUG-Owl2.py
+++ models/load_mPLUG-Owl2.py
@@ -1,47 +1,35 @@
 import torch
 from PIL import Image
 from transformers import TextStreamer
+
 from mplug_owl2.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN
 from mplug_owl2.conversation import conv_templates, SeparatorStyle
 from mplug_owl2.model.builder import load_pretrained_model
-from mplug_owl2.mm_utils import process_images, tokenizer_image_token, get_model_name_from_path, \
-    KeywordsStoppingCriteria
-import json
-import os
-import tqdm
-import random
+from mplug_owl2.mm_utils import process_images, tokenizer_image_token, get_model_name_from_path, KeywordsStoppingCriteria
 
 
+model_path = 'MAGAer13/mplug-owl2-llama2-7b'
 
-# Function to load model
-def load_model(model_path):
-    model_name = get_model_name_from_path(model_path)
-    tokenizer, model, image_processor, context_len = load_pretrained_model(
-        model_path=model_path,
-        model_base=None,
-        model_name=model_name,
-        load_8bit=False,
-        load_4bit=False,
-        device="cuda"
-    )
-    return tokenizer, model, image_processor
+model_name = get_model_name_from_path(model_path)
+tokenizer, model, image_processor, context_len = load_pretrained_model(model_path, None, model_name, load_8bit=False, load_4bit=False, device="cuda")
 
 
-# Function to generate output with mPLUG-Owl
-def generate_with_mplug_owl(tokenizer, model, image_processor, image_path, query):
-    image = Image.open(image_path).convert('RGB')
-    max_edge = max(image.size)
-    image = image.resize((max_edge, max_edge))
-    image_tensor = process_images([image], image_processor).to(model.device, dtype=torch.float16)
-
+def call_model(image_file, query):
     conv = conv_templates["mplug_owl2"].copy()
+    roles = conv.roles
+    image = Image.open(image_file).convert('RGB')
+    max_edge = max(image.size) # We recommand you to resize to squared image for BEST performance.
+    image = image.resize((max_edge, max_edge))
+
+    image_tensor = process_images([image], image_processor)
+    image_tensor = image_tensor.to(model.device, dtype=torch.float16)
+
     inp = DEFAULT_IMAGE_TOKEN + query
     conv.append_message(conv.roles[0], inp)
     conv.append_message(conv.roles[1], None)
     prompt = conv.get_prompt()
 
-    input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(
-        model.device)
+    input_ids = tokenizer_image_token(prompt, tokenizer, IMAGE_TOKEN_INDEX, return_tensors='pt').unsqueeze(0).to(model.device)
     stop_str = conv.sep2
     keywords = [stop_str]
     stopping_criteria = KeywordsStoppingCriteria(keywords, tokenizer, input_ids)
@@ -59,8 +47,6 @@ def generate_with_mplug_owl(tokenizer, model, image_processor, image_path, query
             max_new_tokens=max_new_tokens,
             streamer=streamer,
             use_cache=True,
-            stopping_criteria=[stopping_criteria]
-        )
+            stopping_criteria=[stopping_criteria])
 
-    outputs = tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
-    return outputs
+    return tokenizer.decode(output_ids[0, input_ids.shape[1]:]).strip()
